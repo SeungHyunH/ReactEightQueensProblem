d0da65119b8b9c5b60d1b16ca4894fc57fa75889
SeungHyunH <vkflek1232@naver.com>
2022-07-03 16:19:12 +0900

    수정중 (모든 해를 탐색하도록 구현필요)

--- react_eight_queens_problem/src/components/Board.jsx
+++ react_eight_queens_problem/src/components/Board.jsx
@@ -4,17 +4,7 @@ const Board = () => {
   return (
     <>
       <div>Board</div>
-      <button onClick={()=>{
-        const BoardSize = 5;
-        let result = util.solve(BoardSize,[[0,0]]);
-        let count = 1;
-        while(result[0][0][1]+1 < BoardSize){
-          result = util.solve(BoardSize,[[0,result[0][0][1]+1]]);
-          if(result === -1){break;}
-          count+=1;
-        }
-        console.log(count);
-      }}>TEST</button>
+      <button onClick={()=>{console.log(util.solve(5)[0].length);}}>TEST</button>
     </>
   )
 }
--- react_eight_queens_problem/src/util/solve.js
+++ react_eight_queens_problem/src/util/solve.js
@@ -1,30 +1,35 @@
-export function solve(BOARD_SIZE,stack){
-    let stackPath = [[...stack.at(-1),'Push']];
-    while(stack.length < BOARD_SIZE){
-        let isBacktracking = true;
-        let i = 0;
-        for(; i < BOARD_SIZE; i++){
-            let isNext = true;
-            for(let j = 0; j < stack.length; j++){
-                if(stack[j][1]===i){isNext=false; break;}//직선
-                if(Math.abs(stack[j][0]-stack.length)===Math.abs(stack[j][1]-i)){isNext=false; break;}//대각선
-            }
-            if(isNext){isBacktracking=false; break;}
-        }
-        if(isBacktracking){
-            let result = Backtracking(stack,stackPath,BOARD_SIZE);
-            if(result === -1){
-                return -1;
-            }else{
-                stack = [...result[0]];
-                stackPath = [...result[1]];
-            }
-        }else{
-            stack.push([stack.length,i]);
-            stackPath.push([...stack.at(-1),'Push']);
-        }
-    }
-    return [stack,stackPath];
+export function solve(BOARD_SIZE){
+    const solutionList = [];
+    const pathList = [];
+
+    
+
+    // let stackPath = [[...stack.at(-1),'Push']];
+    // while(stack.length < BOARD_SIZE){
+    //     let isBacktracking = true;
+    //     let i = 0;
+    //     for(; i < BOARD_SIZE; i++){
+    //         let isNext = true;
+    //         for(let j = 0; j < stack.length; j++){
+    //             if(stack[j][1]===i){isNext=false; break;}//직선
+    //             if(Math.abs(stack[j][0]-stack.length)===Math.abs(stack[j][1]-i)){isNext=false; break;}//대각선
+    //         }
+    //         if(isNext){isBacktracking=false; break;}
+    //     }
+    //     if(isBacktracking){
+    //         let result = Backtracking(stack,stackPath,BOARD_SIZE);
+    //         if(result === -1){
+    //             return -1;
+    //         }else{
+    //             stack = [...result[0]];
+    //             stackPath = [...result[1]];
+    //         }
+    //     }else{
+    //         stack.push([stack.length,i]);
+    //         stackPath.push([...stack.at(-1),'Push']);
+    //     }
+    // }
+    return [solutionList,pathList];
 }
 
 function Backtracking(stack,stackPath,BOARD_SIZE){
